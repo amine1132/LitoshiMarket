fa05ef20c8fa4711baa1d31a7e724e838e9961b1
Amine Sekkiou <amine1132@hotmail.fr>
2023-05-26 23:39:51 +0200

    Sheeesh le Search

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -144,10 +144,6 @@ function Dashboard() {
           <p>I hope everything is fine today...</p>
         </div>
         <div className="input">
-          <div className="loupe">
-            <img src={search} alt=""/>
-          </div>
-          <input type="text" placeholder="Search" className="formulaire_1" />
           <button>Connect your wallet</button>
           <div className="notif">
           </div>
