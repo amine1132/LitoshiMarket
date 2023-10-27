a8b57c2fda92a6a8d0cddec1dc4b4c467483ee7c
Amine Sekkiou <amine1132@hotmail.fr>
2023-10-27 15:06:43 +0200

    progress of the explorer token

--- src/dashboard/Explorer/Explorer.jsx
+++ src/dashboard/Explorer/Explorer.jsx
@@ -55,7 +55,7 @@ const chartOptions = {
   },
 };
 
-function Explorer({}) {
+function Explorer({ tokenData }) {
   const [data, setData] = useState([]);
   const [totalMarketCap, setTotalMarketCap] = useState(0.0);
   const [totalVols24h, setTotalVols24h] = useState(0.0);
@@ -203,29 +203,116 @@ function Explorer({}) {
             <div className="w-full flex gap-6">
               <div className="flex flex-col w-3/4 gap-10">
                 <div className="explorer_token_charts"></div>
-                <div className="explorer_token_data"></div>
+                <div className="explorer_token_data">
+                  {showNFTContent ? (
+                    <nav className="topline_1">
+                      <table>
+                        <thead>
+                          <th>Token</th>
+                          <th>Deploy Time</th>
+                          <th>Holders</th>
+                          <th>Transaction</th>
+                          <th>Progress%</th>
+                        </thead>
+                        <tbody className="semi">
+                          {data.map((token, index) => (
+                            <TickComponent2
+                              tokenData={token}
+                              index={index + 1}
+                            />
+                          ))}
+                        </tbody>
+                      </table>
+                    </nav>
+                  ) : showTokenContent ? (
+                    <nav className="topline_1">
+                      <table>
+                        <thead>
+                          <th>Date</th>
+                          <th></th>
+                          <th>Type</th>
+                          <th>USD</th>
+                          <th>$LITE</th>
+                          <th>Price</th>
+                          <th>TXN</th>
+                          <th>Supply</th>
+                        </thead>
+                        <tbody className="semi">
+                          {data.map((token, index) => (
+                            <TickComponent
+                              tokenData={token}
+                              index={index + 1}
+                            />
+                          ))}
+                        </tbody>
+                      </table>
+                    </nav>
+                  ) : showTransactionContent ? (
+                    <nav className="topline_1">
+                      <table>
+                        <thead>
+                          <tr>
+                            <th className="user">User</th>
+                            <th>Net worth</th>
+                            <th>Top token</th>
+                          </tr>
+                        </thead>
+                        <tbody className="semi">
+                          {data.map((token, index) => (
+                            <TickComponent3 key={index} tokenData={token} />
+                          ))}
+                        </tbody>
+                      </table>
+                    </nav>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
               </div>
               <div className="explorer_token_table">
                 <p>LITS/LITE</p>
-                <div>
+                <div className="explorer_token_flex1">
                   <p>Price USD</p>
-                  <p>Marketcap</p>
+                  <p>Marketcap{totalMarketCap}</p>
+                </div>
+                <div className="explorer_token_flex1">
                   <p>5M</p>
                   <p>1H</p>
                   <p>24H</p>
                 </div>
-                <div>
-                  <p>TXNS</p>
-                  <p>Volume</p>
-                  <p>Buys</p>
-                  <p>SELLS</p>
-                  <p>BUY VOL</p>
-                  <p>SELL VOL</p>
+                <div className="explorer_token_flex2">
+                  <div className="flex justify-around	">
+                    <p>TXNS</p>
+                    <p>Volume</p>
+                  </div>
+                  <div className="flex justify-around">
+                    <p>Buys</p>
+                    <p>SELLS</p>
+                  </div>
+                  <div className="flex justify-around">
+                    <p>BUY VOL</p>
+                    <p>SELL VOL</p>
+                  </div>
                 </div>
-                <div>
-                  <button>Watchlist</button>
-                  <button>Alerts</button>
-                  <button>Trade on unilit</button>
+                <div className="flex_justify-content">
+                  <div className="flex flex-col w-full items-center gap-2">
+                    <button>Watchlist</button>
+                    <button>Alerts</button>
+                    <button>Trade on unilit</button>
+                  </div>
+                </div>
+                <div className="token_created">
+                  <p>Token created</p>
+                </div>
+                <div className="flex flex-col gap-5">
+                  <input type="text" className="border-solid border-3" />
+                  <input type="text" className="border-solid" />
+                </div>
+                <div className="flex justify-around">
+                  <button>discord</button>
+                  <button>twitter</button>
+                  <button>github</button>
+                  <button>medium</button>
                 </div>
               </div>
             </div>
@@ -394,7 +481,10 @@ function TickComponent({ tokenData, index, onTableRowClick }) {
 
   return (
     <>
-      <tr onClick={() => onTableRowClick(tokenData.tick)}>
+      <tr
+        onClick={() => onTableRowClick(tokenData.tick)}
+        className="cursor-pointer"
+      >
         <td className="iconoutline">{tokenData.star}</td>
         <td className="number_table">{index}</td>
         <td className="border_bottom">{tokenData.tick.toUpperCase()}</td>
