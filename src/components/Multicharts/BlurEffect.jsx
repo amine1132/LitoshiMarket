2a053df44d740ba7330db4c13becd4fe7fe06ce1
NossWP <williampot@hotmail.fr>
2024-02-07 16:24:40 +0100

    other fx

--- src/components/Multicharts/BlurEffect.jsx
+++
@@ -1,7 +0,0 @@
-export default function setBlurEffect({state}) {
-    const [blurState, setBlurState] = useState(false);
-    if (state == true || state == false) {
-        blurState = state;
-    }
-    return blurState;
-}
