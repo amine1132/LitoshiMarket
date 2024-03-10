c3af6da0a69ba628dcd5822526d19121f1f86151
NossWP <williampot@hotmail.fr>
2024-03-10 14:02:40 +0100

    ffixes

--- src/dashboard/Watchlist/TopButtons.jsx
+++ src/dashboard/Watchlist/TopButtons.jsx
@@ -4,23 +4,23 @@ function TopButtons({ BackGroundColor, SecondColor, additem, addwatch, arrowrigh
             <div className={`flex justify-between`}>
             <button type="" className={`m-1.5 flex self-center justify-between rounded-lg w-full bg-[${BackGroundColor}]`}>
                 <p className="ml-2 my-2">My BRC</p>
-                <img className="mr-2" src={arrowright} alt=""/>
+                <img className="mr-2 my-2" src={arrowright} alt=""/>
             </button>
             <button type="" className={`flex self-center justify-between rounded-lg w-full bg-[${BackGroundColor}]`}>
                 <p className="ml-2 my-2">New Watchlist</p>
-                <img className="mr-2" src={addwatch} alt="" />
+                <img className="mr-2 my-2" src={addwatch} alt="" />
             </button>
             <button type="" className={`m-1.5 flex self-center justify-between rounded-lg w-full bg-[${BackGroundColor}]`}>
                 <p className="ml-2 my-2">Add pair</p>
-                <img className="mr-2" src={additem} alt="" />
+                <img className="mr-2 my-2" src={additem} alt="" />
             </button>
             <button type="" onClick={toggleSetShowManageList} className={`flex self-center justify-between rounded-lg w-full bg-[${BackGroundColor}]`}>
                 <p className="ml-2 my-2">Add an alert</p>
-                <img className="mr-2" src={notification} alt="" />
+                <img className="mr-2 my-2" src={notification} alt="" />
             </button>
             <button type="" onClick={toggleSetShowShareWatchList} className={`m-1.5 flex self-center justify-between rounded-lg w-full bg-[${BackGroundColor}]`}>
                 <p className="ml-2 my-2">Share this watchlist</p>
-                <img className="mr-2" src={Share} alt="" />
+                <img className="mr-2 my-2" src={Share} alt="" />
             </button>
             </div>
         </div>
