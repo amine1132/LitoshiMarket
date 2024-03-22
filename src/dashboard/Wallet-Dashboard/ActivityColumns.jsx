36cfb8225b432a5112e3176a053167ee0e6316e3
yyayourt <yanisnasri30@gmail.com>
2024-03-22 15:25:55 +0100

    progress of wallet-token

--- src/dashboard/Wallet-Dashboard/ActivityColumns.jsx
+++ src/dashboard/Wallet-Dashboard/ActivityColumns.jsx
@@ -1,46 +1,40 @@
 import Activity from "../../Data/Activity.json";
 
 function ActivityColumns({ data, currentPage, itemsPerPage, backgroundColor }) {
-  const startIdx = (currentPage - 1) * itemsPerPage;
-  const endIdx = currentPage * itemsPerPage;
-  const currentData = Activity.slice(startIdx, endIdx);
+    const startIdx = (currentPage - 1) * itemsPerPage;
+    const endIdx = currentPage * itemsPerPage;
+    const currentData = Activity.slice(startIdx, endIdx);
 
-  return (
-    <div className="mt-2">
-      <table className="w-full">
-        <tbody className="">
-          {currentData.map((item, index) => (
-            <tr
-              key={index}
-              className={
-                index === 0 ? null : `border-t border-t-[${backgroundColor}]`
-              }
-            >
-              <td className={"text-gray-600 w-[150px]"}>
-                <p>{item.ts}</p>
-                <p>{item.from_wallet}</p>
-              </td>
-              <td className="flex text-gray-600 text-left">
-                <img
-                  src="/src/assets/notification.svg"
-                  className="h-[45px] w-[45px]"
-                />
-                <div>
-                  <p>{item.sale_price}</p>
-                  <p>{item.to_wallet}</p>
-                </div>
-              </td>
-              <td className="py-4 text-right">{item.data3}</td>
-              <td className="py-4 text-right">
-                <p className="inline text-gray-600">{item.type_of_money} </p>
-                {item.Gas}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="mt-2">
+            <table className="w-full">
+                <tbody className="">
+                    {currentData.map((item, index) => (
+                        <tr key={index} className={index === 0 ? null : "border-t"}>
+                            <td className={"w-[150px]"} style={{ color: "rgb(107, 106, 109)" }}>
+                                <p>{item.ts}</p>
+                                <p>{item.from_wallet}</p>
+                            </td>
+                            <td className="flex text-left ml-4" style={{ color: "rgb(107, 106, 109)" }}>
+                                <img src="/src/assets/notification.svg" className="h-[45px] w-[45px]" />
+                                <div>
+                                    <p>{item.sale_price}</p>
+                                    <p>{item.to_wallet}</p>
+                                </div>
+                            </td>
+                            <td className="py-4 text-right">{item.data3}</td>
+                            <td className="py-4 text-right">
+                                <p className="inline" style={{ color: "rgb(107, 106, 109)" }}>
+                                    {item.type_of_money}{" "}
+                                </p>
+                                {item.Gas}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default ActivityColumns;
--- src/dashboard/Wallet-Dashboard/NftGesture.jsx
+++ src/dashboard/Wallet-Dashboard/NftGesture.jsx
@@ -1,35 +1,26 @@
 import React from "react";
 import Nftdata from "../../Data/NFT.json";
 import "./NFT.css";
+import blur from "../../assets/wallets/blur.svg";
 
-function generateNFTElements(
-  count,
-  isBiggerButtonClicked,
-  selectedNFTPage,
-  itemsPerPageNFT
-) {
-  // Sélectionnez les éléments à afficher en fonction de la pagination
-  const slicedData = Nftdata.slice(
-    isBiggerButtonClicked ? 0 : (selectedNFTPage - 1) * itemsPerPageNFT,
-    isBiggerButtonClicked ? count : selectedNFTPage * itemsPerPageNFT
-  );
-  console.log(Nftdata);
-  return slicedData.map((NFT, index) => (
-    <div
-      className={isBiggerButtonClicked ? "w-[20%] mb-4" : "w-[20%]"}
-      key={index}
-    >
-      <div
-        className={`rounded-lg h-[250px] max-w-[240px] bg-[#151516] relative`}
-      >
-        <div className="nft_blur"></div>
-        <img className="nft_image" src={NFT.content_url} />
-        <p className="absolute top-[85%] font-bold z-10">
-          {NFT.inscription_name}
-        </p>
-      </div>
-    </div>
-  ));
+function generateNFTElements(count, isBiggerButtonClicked, selectedNFTPage, itemsPerPageNFT) {
+    // Sélectionnez les éléments à afficher en fonction de la pagination
+    const slicedData = Nftdata.slice(
+        isBiggerButtonClicked ? 0 : (selectedNFTPage - 1) * itemsPerPageNFT,
+        isBiggerButtonClicked ? count : selectedNFTPage * itemsPerPageNFT
+    );
+    console.log(Nftdata);
+    return slicedData.map((NFT, index) => (
+        <div className={isBiggerButtonClicked ? "w-[20%] mb-4" : "w-[20%]"} key={index}>
+            <div className={`rounded-lg h-[250px] max-w-[240px] bg-[#151516] relative`}>
+                <div class="nft_blur"></div>
+
+                <img className="nft_image" src={NFT.content_url} />
+
+                <p className="absolute top-[85%] font-bold z-10">{NFT.inscription_name}</p>
+            </div>
+        </div>
+    ));
 }
 
 export default generateNFTElements;
