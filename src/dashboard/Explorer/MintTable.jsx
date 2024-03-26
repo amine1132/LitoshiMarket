7a796ef47cb8a1fa4790470bd41cba1acce24a4e
Amine Sekkiou <amine1132@hotmail.fr>
2024-03-26 14:20:53 +0100

    Mint progress

--- src/dashboard/Explorer/MintTable.jsx
+++ src/dashboard/Explorer/MintTable.jsx
@@ -1,65 +1,74 @@
 import React from "react";
 import Pagination from "./Pagination";
+import datamarket from "../../Data/Explorer/Marketdata.json";
 
-function MintTable({ data, BackGroundColor }){
-
+function MintTable({ data, BackGroundColor }) {
   const [currentPage, setCurrentPage] = React.useState(1);
   const itemsPerPage = 12;
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = datamarket.slice(indexOfFirstItem, indexOfLastItem);
 
   const totalPages = Math.ceil(data.length / itemsPerPage);
   // END PAGINATION
 
   return (
-  <>
-  <nav className="overflow-y-auto overflow-x-hidden h-[600px]">
-    <table className="w-full">
-      <thead className="text-xs text-left h-[40px]">
-        <th>Token</th>
-        <th>Deploy Time</th>
-        <th>Holders</th>
-        <th>Transaction</th>
-        <th>Progress%</th>
-      </thead>
-      <tbody className="semi">
-        {currentItems.map((token) => (
-          <tr className="h-[40px]">
+    <>
+      <nav className="overflow-y-auto overflow-x-hidden h-[600px]">
+        <table className="w-full">
+          <thead className="text-xs text-left h-[40px]">
+            <th>Token</th>
+            <th>Deploy Time</th>
+            <th>Holders</th>
+            <th>Transaction</th>
+            <th>Progress%</th>
+          </thead>
+          <tbody className="semi">
+            {currentItems.map((token) => (
+              <tr className="h-[40px]">
+                <td className="">{token.ticker}</td>
+                <td className="">{token.deploy_ts}</td>
+                <td
+                  className={
+                    token.holder_count && parseFloat(token.holder_count) < 0
+                      ? "negative"
+                      : token.holder_count && parseFloat(token.holder_count) > 0
+                      ? "positive"
+                      : "na"
+                  }
+                >
+                  {token.holder_count
+                    ? (parseFloat(token.holder_count) >= 0 ? "+" : "") +
+                      parseFloat(token.holder_count).toFixed(2) +
+                      "%"
+                    : "N/A"}
+                </td>
+                <td className="">
+                  {Number(token.vol_1d).toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                    maximumFractionDigits: 0,
+                  })}
+                </td>
+                <td className="">
+                  <div>100%</div>
+                  <div className="progressbar"></div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </nav>
 
-            <td className="">{token.tick.toUpperCase()}</td>
-            <td className="">25 May 2023 22:38:40</td>
-            <td
-              className={
-                token.change_24h && parseFloat(token.change_24h) < 0
-                  ? "negative"
-                  : token.change_24h && parseFloat(token.change_24h) > 0
-                    ? "positive"
-                    : "na"
-              }
-            >
-              {token.change_24h
-                ? (parseFloat(token.change_24h) >= 0 ? "+" : "") +  parseFloat(token.change_24h).toFixed(2) + "%"
-                : "N/A"}
-            </td>
-            <td className="">
-              {Number(token.vol_24h).toLocaleString("en-US", {  style: "currency",  currency: "USD",  maximumFractionDigits: 0,  })}
-            </td>
-            <td className="">
-              <div>100%</div>
-              <div className="progressbar"></div>
-            </td>
-
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </nav>
-
-  <Pagination currentPage={currentPage} totalPages={totalPages} setCurrentPage={setCurrentPage} backgroundColor={BackGroundColor} />
-  </>
-  )
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+        backgroundColor={BackGroundColor}
+      />
+    </>
+  );
 }
 
 export default MintTable;
